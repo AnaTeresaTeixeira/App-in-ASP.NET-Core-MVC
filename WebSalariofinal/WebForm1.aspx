18162ce39f753b5b95383b60e584196e8de1f8a2
Ana Teixeira <96289344+AnaTeresaTeixeira@users.noreply.github.com>
2023-03-07 11:56:06 +0000

    Add files via upload

---
+++ WebSalariofinal/WebForm1.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebSalariofinal.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            Cálculo de Salário
+            <br />
+            <br />
+            -----------------------------------------------------------------------------------------------------------------------------------------<br />
+            <br />
+            Salário bruto     
+            <asp:TextBox ID="TextBox1" runat="server" style="margin-left: 28px; margin-top: 0px;" Width="160px"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="margin-left: 37px; margin-top: 0px" Text="Calcular" Width="109px" />
+            <br />
+            <br />
+            Segurança Social
+            <asp:TextBox ID="TextBox2" runat="server" style="margin-left: 28px" Width="160px"></asp:TextBox>
+            <br />
+            <br />
+            IRS  -   <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            &nbsp;
+            <br />
+            <br />
+            Valor IRS  -    <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+            <br />
+            <br />
+            Salário líquido
+            <asp:TextBox ID="TextBox3" runat="server" style="margin-left: 28px" Width="160px"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebSalariofinal/WebForm1.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebSalariofinal
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = "";
+            Label2.Text = "";
+            Label3.Text = "";
+            TextBox2.ReadOnly = true;
+            TextBox3.ReadOnly = true;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            float salario = 0F, segSocial = 0F, liquido = 0F;
+
+            if((TextBox1.Text.Trim()).Length > 0)
+            {
+                try
+                {
+                    salario = float.Parse("0" + TextBox1.Text);
+                    segSocial = salario * 0.2f;
+                    TextBox2.Text = segSocial.ToString();
+
+                    if(salario<500)
+                    {
+                        Label1.Text = "0";
+                        Label2.Text = "0";
+                        liquido = salario - segSocial;
+                    }
+                    else if(salario >= 500 && salario < 1000)
+                    {
+                        Label1.Text = "12";
+                        Label2.Text = (salario * 0.12F).ToString();
+                        liquido = salario - segSocial - salario * 0.12F;
+                    }
+                    else if(salario >= 1000 && salario < 1500)
+                    {
+                        Label1.Text = "15";
+                        Label2.Text = (salario * 0.15F).ToString();
+                        liquido = salario - segSocial - salario * 0.15F;
+                    }
+                    else
+                    {
+                        Label1.Text = "18";
+                        Label2.Text = (salario * 0.18F).ToString();
+                        liquido = salario - segSocial - salario * 0.18F;
+                    }
+                    Label3.ForeColor = System.Drawing.Color.Green;
+                    Label3.Text = (salario.ToString() + " - " + segSocial.ToString() + " - " + Label2.Text);
+                    TextBox3.Text = liquido.ToString();
+                }
+                catch
+                {
+                    Limpar();
+                    Label3.ForeColor = System.Drawing.Color.Red;
+                    Label3.Text = "Erro!";
+                }
+            } else {
+                Limpar();
+                Label3.ForeColor = System.Drawing.Color.RoyalBlue;
+                Label3.Text = "Indique um valor!";
+            }
+        }
+
+        void Limpar()
+        {
+            Label1.Text = "";
+            Label2.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+        }
+    }
+}
